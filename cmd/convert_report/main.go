d60e16c8b7bcc3416a4b8ab043bab2a0a2a50f7b
Vladimir <vxvxn@yandex.ru>
2023-05-20 11:18:39 +0300

    add auto adding name of output file for convert_report

--- cmd/convert_report/main.go
+++ cmd/convert_report/main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
+	"github.com/VxVxN/market_analyzer/internal/saver/csvsaver"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/VxVxN/market_analyzer/internal/parser/smartlabparser"
-	"github.com/VxVxN/market_analyzer/internal/saver/csvsaver"
 )
 
 func main() {
 	if len(os.Args) != 3 {
 		log.Fatalln("two arguments were expected")
 	}
+	inputFile := os.Args[1]
 	parser := smartlabparser.Init()
-	parser.SetFilePath(os.Args[1])
+	parser.SetFilePath(inputFile)
 	data, err := parser.Parse()
 	if err != nil {
 		log.Fatalln(err)
 	}
-	saver := csvsaver.Init(os.Args[2], data.Headers, data.Rows)
+	fileOutput := filepath.Join(os.Args[2], filepath.Base(inputFile))
+	saver := csvsaver.Init(fileOutput, data.Headers, data.Rows)
 	if err = saver.Save(); err != nil {
 		log.Fatalln(err)
 	}
